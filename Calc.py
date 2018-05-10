6d3d715eb8a7c997e8f3175128a672c2dc63e0c3
PI42 <39027971+PI42@users.noreply.github.com>
2018-05-10 13:42:50 +0700

    ...

--- Calc.py
+++ Calc.py
@@ -1,7 +1,13 @@
 from tkinter import *
  
-root = Tk()
-root.title("Калькулятор")
-root.geometry("400x300")
- 
-root.mainloop()
+TK = Tk()
+tv = StringVar()
+Label(TK, textvariable=tv, relief="groove", borderwidth=3, font=("Courier", 20, "bold"), justify=LEFT, width=50, padx=10, pady=20, takefocus=False,).pack()
+Entry(TK, textvariable=tv, takefocus=True,).pack()
+ #tv.set("123")
+
+
+TK.title("Калькулятор")
+TK.geometry("400x300")
+
+TK.mainloop()
