314ce912f24016ae6b740732220b5f89ce535132
PI42 <39027971+PI42@users.noreply.github.com>
2018-05-10 16:23:06 +0700

    ...

--- Calc.py
+++ Calc.py
@@ -1,15 +1,24 @@
 from tkinter import *
+
 AAA = int()
 BBB = int()
 TK = Tk()
 tv = StringVar()
 Rezult = int()
 RezultView = StringVar()
+
+
+Label(text="A", padx="15", pady="6", font="15").pack()
 Entry(TK, takefocus=True,).pack()
+Label(text="B", padx="15", pady="6", font="15").pack()
 Entry(TK, takefocus=True,).pack()
+
+
 tv.set("=")
+
 Label(TK, textvariable=tv, relief="groove", borderwidth=0, font=("Courier", 20, "bold"), justify=LEFT, width=30, padx=10, pady=10, takefocus=False,).pack()
-Label(TK, textvariable=RezultView, relief="groove", borderwidth=0, font=("Courier", 20, "bold"), justify=LEFT, width=30, padx=10, pady=10, takefocus=False,).pack()
+LabelRezult = Label(TK, textvariable=RezultView, relief="groove", borderwidth=2, font=("Courier", 20, "bold"), justify=LEFT, width=30, padx=10, pady=10, takefocus=False,)
+LabelRezult.pack()
 
 BtnPlus = Button(text="+", background="#555", foreground="#ccc", padx="15", pady="6", font="15")
 BtnPlus.pack(side=LEFT, fill=NONE)
@@ -23,5 +32,4 @@ BtnRazdel.pack(side=LEFT, fill=NONE)
 
 TK.title("Калькулятор")
 TK.geometry("400x300")
-
 TK.mainloop()
