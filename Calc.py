a41981953fb21d52eb53973da45210c38bc0c51b
PI42 <39027971+PI42@users.noreply.github.com>
2018-05-10 15:44:41 +0700

    ...

--- Calc.py
+++ Calc.py
@@ -1,10 +1,24 @@
 from tkinter import *
- 
+AAA = int()
+BBB = int()
 TK = Tk()
 tv = StringVar()
-Label(TK, textvariable=tv, relief="groove", borderwidth=3, font=("Courier", 20, "bold"), justify=LEFT, width=50, padx=10, pady=20, takefocus=False,).pack()
-Entry(TK, textvariable=tv, takefocus=True,).pack()
- #tv.set("123")
+Rezult = int()
+RezultView = StringVar()
+Entry(TK, takefocus=True,).pack()
+Entry(TK, takefocus=True,).pack()
+tv.set("=")
+Label(TK, textvariable=tv, relief="groove", borderwidth=0, font=("Courier", 20, "bold"), justify=LEFT, width=30, padx=10, pady=10, takefocus=False,).pack()
+Label(TK, textvariable=RezultView, relief="groove", borderwidth=0, font=("Courier", 20, "bold"), justify=LEFT, width=30, padx=10, pady=10, takefocus=False,).pack()
+
+BtnPlus = Button(text="+", background="#555", foreground="#ccc", padx="15", pady="6", font="15")
+BtnPlus.pack(side=LEFT, fill=NONE)
+BtnMinus = Button(text="-", background="#555", foreground="#ccc", padx="15", pady="6", font="15")
+BtnMinus.pack(side=LEFT, fill=NONE)
+BtnUmnoz = Button(text="*", background="#555", foreground="#ccc", padx="15", pady="6", font="15")
+BtnUmnoz.pack(side=LEFT, fill=NONE)
+BtnRazdel = Button(text="/", background="#555", foreground="#ccc", padx="15", pady="6", font="15")
+BtnRazdel.pack(side=LEFT, fill=NONE)
 
 
 TK.title("Калькулятор")
